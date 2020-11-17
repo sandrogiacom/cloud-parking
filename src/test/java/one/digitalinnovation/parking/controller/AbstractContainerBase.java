8c44378322ae519ba98215f559be2e8f45813074
Sandro Giacomozzi <sandrogiacom@gmail.com>
2020-11-17 19:00:51 -0300

    Adicionando testcontainers

---
+++ src/test/java/one/digitalinnovation/parking/controller/AbstractContainerBase.java
@@ -0,0 +1,16 @@
+package one.digitalinnovation.parking.controller;
+
+import org.testcontainers.containers.PostgreSQLContainer;
+
+public abstract class AbstractContainerBase {
+
+    static final PostgreSQLContainer POSTGRE_SQL_CONTAINER;
+
+    static {
+        POSTGRE_SQL_CONTAINER = new PostgreSQLContainer("postgres:10-alpine");
+        POSTGRE_SQL_CONTAINER.start();
+        System.setProperty("spring.datasource.url", POSTGRE_SQL_CONTAINER.getJdbcUrl());
+        System.setProperty("spring.datasource.username", POSTGRE_SQL_CONTAINER.getUsername());
+        System.setProperty("spring.datasource.password", POSTGRE_SQL_CONTAINER.getPassword());
+    }
+}
